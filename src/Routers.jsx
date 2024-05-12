6f6e3d00380917bfc8c32103c7d2db90982eae14
ARTcalm <167033039+ARTcalm@users.noreply.github.com>
2024-05-12 15:33:35 +0300

    Update Routers.jsx

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,24 +1,24 @@
 import {Route, Switch} from "react-router-dom/cjs/react-router-dom.min"
-import {Main} from './components/Main/Main'
+import {Main} from './components/Main/MainWarehouse'
 import {Layout} from "./components/Layout/Layout"
 import {useState} from "react";
+import { Auction } from "./components/Auction/MainAuction";
+import { AboutUs } from "./components/AboutUs/MainAboutUs";
 
 export const Routers = () => {
 
     const [cartItems, setCartItems] = useState([])
 
     const renderMain = () => <Main setCartItems={setCartItems} cartItems={cartItems} />
+    const renderAuction = () => <Auction />
+    const renderAboutUs = () => <AboutUs/>
 
     return <div><Layout cartItems={cartItems} setCartItems={setCartItems} />
         <Switch>
             <Route path="/" exact render={() => renderMain()}/>
             <Route path="/sklad"  render={() => renderMain()}/>
-            <Route path="/torgi" render={() => {
-            }}/>
-            <Route path="/aboutus" render={() => {
-            }}/>
-            <Route path="/contact" render={() => {
-            }}/>
+            <Route path="/torgi" render={() => renderAuction()}/>
+            <Route path="/aboutus" render={() => renderAboutUs()}/>
         </Switch>
     </div>
 }
